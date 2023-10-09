32f6eb5b71779f65d4f0fd0bc40487786052ad18
kimhyun <forhn37@gmail.com>
2023-10-09 17:34:35 +0900

    동기방식 예제

---
+++ exam.js
@@ -0,0 +1,14 @@
+function buy_normal(item, price, quantity) {
+  console.log(`${item}상품을 ${quantity}개 골라서 점원에게 주었습니다.`);
+  console.log("계산이 필요합니다");
+  let total = price*quantity;
+  return total;
+}
+
+function pay(tot) {
+  console.log(`${tot}원을 지불하였습니다.`);
+}
+let tot = buy_normal("고구마", 1000, 5);
+pay(tot);
+
+// 동기방식 예제
