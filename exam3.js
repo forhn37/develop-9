32ae16ba8e54f4d4fd2adf8e004032aea909b4b3
kimhyun <forhn37@gmail.com>
2023-10-09 23:08:51 +0900

    exam3 입코딩 완료

--- exam3.js
+++ exam3.js
@@ -14,3 +14,4 @@ function pay(tot) {
 buy_tobe("고구마", 1000, 5, pay);
 
 // buy_tobe실행 후 callback처리 그 후 마지막 펑션 진행
+// buy_tobe("고구마", 1000, 5, pay);실행 후 console.log(`${item}상품을${quantity}개 골라서 점원에게 주었습니다.`); 출력 후 setTimeout을 호출한다 1초 후에 console.log("계산이 필요합니다"); 출력 후 total을 매개변수로 가진 pay함수가 실행된다. 
